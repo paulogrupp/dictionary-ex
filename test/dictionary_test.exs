de195abc087bbc12af7a31491f491ea30c3bcdfd
Paulo Rupp <paulo.rupp198@gmail.com>
2023-03-15 12:54:39 -0300

    add test

--- test/dictionary_test.exs
+++ test/dictionary_test.exs
@@ -2,7 +2,12 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "returns a word with the specified length" do
+    word_lengths = [5, 7, 10]
+
+    Enum.each(word_lengths, fn word_length ->
+      word = Dictionary.random_word(word_length)
+      assert String.length(word) == word_length
+    end)
   end
 end
